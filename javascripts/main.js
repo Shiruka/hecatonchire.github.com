885c85fa406966e654382ad1b18c3b316a9238e6
Peter Izsak <peter.izsak@sap.com>
2013-04-10 16:20:02 +0300

    New site design

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,25 @@
 $(function(){
-        $("#slides").slides({
-            preload: true,
-            preloadImage: 'images/livemigration.png',
-            play:5000,
-            pause:5000,
-            hoverPause: true,
-            });
+        // $("#slides").slidesjs({
+        //     width: 300,
+        //     height: 200,
+        //     navigation: false,
+        //     pagination: false,
+            
+        //     });
+
+        $("#nav_docs").click(makeNavFunc('nav_docs', 'docs.html'));
+        $("#nav_download").click(makeNavFunc('nav_download', 'download.html'));
+        $("#nav_news").click(makeNavFunc('nav_news', 'news.html'));
+        $("#nav_contant").click(makeNavFunc('nav_contant', 'contact.html'));
     });
+
+function makeNavFunc(name, page) {
+    var selector_name = "#" + name;
+    function nav() {
+        $('#main_content').load(page);
+        $("#main-menu-bar").find("li").removeClass("active");
+        $(selector_name).parent().addClass("active");
+        $("#collape-btn").click();
+    }
+    return nav;
+}
